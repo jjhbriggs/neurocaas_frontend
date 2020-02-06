34989b4320ac982e686a132dcaf486ace04509ba
upworka0 <upworka0@gmail.com>
2020-02-05 17:37:54 -0700

    added account app

--- manage.py
+++ manage.py
@@ -12,4 +12,8 @@ if __name__ == '__main__':
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+
+    if sys.argv.__len__() == 1:
+        sys.argv.append('runserver')
+
     execute_from_command_line(sys.argv)
