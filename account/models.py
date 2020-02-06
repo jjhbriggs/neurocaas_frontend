5cebd07db7c070f212a03876ebffeceb95a09ab8
upworka0 <upworka0@gmail.com>
2020-02-05 20:13:22 -0700

    change db

--- account/models.py
+++ account/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class IAM(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    username = models.CharField(max_length=155)
+    access_key = models.CharField(max_length=255)
+    secret_access_key = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.username
+
--- ncap/settings.py
+++ ncap/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'ncap.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'captivise',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '',
     }
 }
 
