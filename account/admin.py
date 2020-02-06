e09caa0f5af6da3f6144ed8140216ee73840fd21
upworka0 <upworka0@gmail.com>
2020-02-05 21:43:53 -0700

    create custom user model

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import Group
+from .forms import *
+from .models import *
 # Register your models here.
+
+
+class UserAdministrator(UserAdmin):
+    # The forms to add and change user instances
+    form = UserChangeForm
+    add_form = UserCreationForm
+
+    list_display = ('email', 'is_admin', 'first_name', 'last_name')
+    list_filter = ('is_admin',)
+    fieldsets = (
+        (None, {'fields': ('email', 'password', 'first_name', 'last_name')}),
+        ('Permissions', {'fields': ('is_admin',)}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'first_name', 'last_name', 'is_admin')}
+        ),
+    )
+    search_fields = ('email',)
+    ordering = ('email',)
+    filter_horizontal = ()
+
+
+admin.site.register(User, UserAdministrator)
+admin.site.unregister(Group)
+admin.site.register(IAM)
