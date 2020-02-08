615596d859c90da67f73e30dc3614b17d4535915
upworka0 <upworka0@gmail.com>
2020-02-07 21:37:09 -0700

    added main app

--- ncap/settings.py
+++ ncap/settings.py
@@ -35,7 +35,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'account'
+    'account',
+    'main',
 ]
 
 MIDDLEWARE = [
--- ncap/urls.py
+++ ncap/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    path('', include("main.urls")),
     path('admin/', admin.site.urls),
     path('account/', include("account.urls")),
     path('login/', auth_views.LoginView.as_view(template_name="account/login.html"), name="login"),
