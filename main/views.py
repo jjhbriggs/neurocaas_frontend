ada684e6c72e31ff95b96726c5aca63f4f5d653d
upworka0 <upworka0@gmail.com>
2020-02-13 10:35:16 -0700

    finished multipart upload

--- main/views.py
+++ main/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView, View
-
+from django.contrib.auth.mixins import LoginRequiredMixin
 # Create your views here.
 
 
@@ -8,7 +8,25 @@ class HomeView(TemplateView):
     template_name = "main/home.html"
 
 
-class FileUploadView(View):
+class FileUploadView(LoginRequiredMixin, View):
+    """
+        S3 Bucket Permission for CORS
+
+            <?xml version="1.0" encoding="UTF-8"?>
+            <CORSConfiguration xmlns="http://s3.amazonaws.com/doc/2006-03-01/">
+            <CORSRule>
+                <AllowedOrigin>*</AllowedOrigin>
+                <AllowedMethod>GET</AllowedMethod>
+                <AllowedMethod>PUT</AllowedMethod>
+                <AllowedMethod>POST</AllowedMethod>
+                <AllowedMethod>DELETE</AllowedMethod>
+                <MaxAgeSeconds>3000</MaxAgeSeconds>
+                <ExposeHeader>ETag</ExposeHeader>
+                <AllowedHeader>*</AllowedHeader>
+            </CORSRule>
+            </CORSConfiguration>
+
+    """
     template_name = "main/file_upload.html"
 
     def get(self, request):
