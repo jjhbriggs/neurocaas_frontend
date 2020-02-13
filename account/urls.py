7312013e918e62079f5e3667d23bad6f8cfa2ff4
upworka0 <upworka0@gmail.com>
2020-02-12 19:38:16 -0700

    change the login type

--- account/urls.py
+++ account/urls.py
@@ -6,9 +6,10 @@ from account.forms import UserLoginForm
 
 urlpatterns = [
     path('profile/', ProfileView.as_view(), name='profile'),
-    path('login/', auth_views.LoginView.as_view(template_name="account/login.html", authentication_form=UserLoginForm),
-         name="login"),
+    # path('login/', auth_views.LoginView.as_view(template_name="account/login.html", authentication_form=UserLoginForm),
+    #      name="login"),
+    path('login/', LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name="logout"),
     path('aws_cred_request/', AWSCredRequestView.as_view(), name='aws_cred_request'),
-    path('user_password_change/', ChangePWDView.as_view(), name='user_password_change')
+    # path('user_password_change/', ChangePWDView.as_view(), name='user_password_change')
 ]
