24a638e1b225e33ae43a53048985648f26616481
upworka0 <upworka0@gmail.com>
2020-02-17 06:38:41 -0700

    working in main app

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+
+@admin.register(Bucket)
+class BucketAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created_on',)
+
+
+@admin.register(FileItem)
+class FileItemAdmin(admin.ModelAdmin):
+    list_display = ('name', 'link', 'uploaded',)
