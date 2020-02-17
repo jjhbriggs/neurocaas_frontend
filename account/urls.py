2487696721cfa3b8da3b34fe3ac5a39b4d1b5514
upworka0 <upworka0@gmail.com>
2020-02-17 06:38:41 -0700

    working in main app

--- account/urls.py
+++ account/urls.py
@@ -1,16 +1,14 @@
 from django.urls import path, include
 from .views import *
 from django.contrib.auth import views as auth_views
-from account.forms import UserLoginForm
+from django.views.generic import TemplateView
 
 
 urlpatterns = [
     path('profile/', ProfileView.as_view(), name='profile'),
-    # path('login/', auth_views.LoginView.as_view(template_name="account/login.html", authentication_form=UserLoginForm),
-    #      name="login"),
     path('signup/', SignUpView.as_view(), name='signup'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name="logout"),
     path('aws_cred_request/', AWSCredRequestView.as_view(), name='aws_cred_request'),
-    # path('user_password_change/', ChangePWDView.as_view(), name='user_password_change')
+    path('instruction/', TemplateView.as_view(template_name="account/instruction.html"))
 ]
