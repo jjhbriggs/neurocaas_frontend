5809d43ed600db94bb6d6eccd17d181da565e276
upworka0 <upworka0@gmail.com>
2020-02-18 10:45:34 -0700

    added completed message

--- static/js/file_upload.js
+++ static/js/file_upload.js
@@ -29,6 +29,7 @@ FileUpload.prototype ={
     multiPartParams: null,
     multipartMap: { Parts: []},
     s3: null,
+    status: false,
 
     init: function(form_id, id1, id2, bucket, subfolder){
         this.form_id = form_id;
@@ -62,7 +63,7 @@ FileUpload.prototype ={
                       console.log(err);
                     } else {
                       var delta = (new Date() - sender.startTime) / 1000;
-                      alert("Finished loading!");
+                      //alert("Finished loading!");
                       console.log('Completed upload in', delta, 'seconds');
                       console.log('Final upload data:', data);
                     }
@@ -94,7 +95,7 @@ FileUpload.prototype ={
 
                     var doneParams = {
                         Bucket: _this.bucket,
-                        Key: _this.fileKey,
+                        Key: _this.subfolder + "/" + _this.fileKey,
                         MultipartUpload: _this.multipartMap,
                         UploadId: multipart.UploadId
                     };
@@ -102,6 +103,8 @@ FileUpload.prototype ={
                     console.log("Completing upload...");
                     completeMultipartUpload(s3, doneParams);
                     updateProgress(_this);
+                    _this.status = true;
+                    $('#' + _this.form_id + ' p').html("Uploading was finished!");
                     $("#upload").attr("disabled", false);
                 });
             }
@@ -126,6 +129,7 @@ FileUpload.prototype ={
 
         // On Click event when user click drop area
         this.dropArea.addEventListener('click', function(e){
+            if (sender.status) return;
             $('#' + sender.form_id + ' .fileElem').trigger('click');
         }, false);
 
@@ -176,7 +180,7 @@ FileUpload.prototype ={
                 sender.maxUploadTries = 3;
                 sender.multiPartParams = {
                     Bucket: sender.bucket,
-                    Key: sender.fileKey,
+                    Key: sender.subfolder + "/" + sender.fileKey,
                     ContentType: file.type
                 };
                 var multipartMap = {
@@ -195,7 +199,7 @@ FileUpload.prototype ={
                             partParams = {
                               Body: sender.buffer.slice(rangeStart, end),
                               Bucket: sender.bucket,
-                              Key: sender.fileKey,
+                              Key: sender.subfolder + "/" + sender.fileKey,
                               PartNumber: String(sender.partNum),
                               UploadId: multipart.UploadId
                             };
@@ -207,30 +211,6 @@ FileUpload.prototype ={
                     }
                 });
             }
-
-            /*var url = 'https://api.cloudinary.com/v1_1/joezimim007/image/upload'
-            var xhr = new XMLHttpRequest()
-            var formData = new FormData()
-            xhr.open('POST', url, true)
-            xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest')
-
-            // Update progress (can be used to show progress indicator)
-            xhr.upload.addEventListener("progress", function(e) {
-                updateProgress(i, (e.loaded * 100.0 / e.total) || 100)
-            })
-
-            xhr.addEventListener('readystatechange', function(e) {
-                if (xhr.readyState == 4 && xhr.status == 200) {
-                  updateProgress(i, 100) // <- Add this
-                }
-                else if (xhr.readyState == 4 && xhr.status != 200) {
-                  // Error. Inform the user
-                }
-            })
-
-            formData.append('upload_preset', 'ujpu6gyk')
-            formData.append('file', file)
-            xhr.send(formData)*/
         }
 
         // Handle files of file tag
@@ -247,6 +227,7 @@ FileUpload.prototype ={
 
         // Handle dropped files
         this.dropArea.addEventListener('drop', function(e){
+            if (sender.status) return;
             var dt = e.dataTransfer
             var files = dt.files
             handleFiles(files)
