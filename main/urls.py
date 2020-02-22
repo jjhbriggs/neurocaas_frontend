e28dad78103cbd5272506f38a82003804c46c54f
upworka0 <upworka0@gmail.com>
2020-02-22 03:39:49 -0700

    remove unnecessary fields

--- main/urls.py
+++ main/urls.py
@@ -6,13 +6,6 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('', TemplateView.as_view(template_name="main/home.html"), name="home"),
 
-    path('step1/', SelectBucketView.as_view(), name='select_bucket'),
-    path('step2/', FileUploadView.as_view(), name='file_upload'),
-    path('step3/', ProcessView.as_view(), name='process'),
-    path('result/', ResultView.as_view(), name='result'),
-    # path('file_upload/', FileUploadView.as_view(), name='file_upload'),
-    path('check_process/', CheckProcessView.as_view(), name='check_process'),
-
     #""" demo """
     path('demo/', DemoView.as_view(), name='demo'),
     path('demo_result/', DemoResultView.as_view(), name='demo_result'),
--- main/views.py
+++ main/views.py
@@ -9,110 +9,14 @@ from .demo_utils import *
 # Create your views here.
 
 
-class SelectBucketView(LoginRequiredMixin, View):
-    """
-        S3 Bucket Permission for CORS
-
-            <?xml version="1.0" encoding="UTF-8"?>
-            <CORSConfiguration xmlns="http://s3.amazonaws.com/doc/2006-03-01/">
-            <CORSRule>
-                <AllowedOrigin>*</AllowedOrigin>
-                <AllowedMethod>GET</AllowedMethod>
-                <AllowedMethod>PUT</AllowedMethod>
-                <AllowedMethod>POST</AllowedMethod>
-                <AllowedMethod>DELETE</AllowedMethod>
-                <MaxAgeSeconds>3000</MaxAgeSeconds>
-                <ExposeHeader>ETag</ExposeHeader>
-                <AllowedHeader>*</AllowedHeader>
-            </CORSRule>
-            </CORSConfiguration>
-
-        Step 1 View
-    """
-    template_name = "main/step1.html"
-
-    def get(self, request):
-        buckets = Bucket.objects.all().order_by('name')
-        return render(request=request, template_name=self.template_name, context={
-            "buckets": buckets,
-        })
-
-
-class FileUploadView(LoginRequiredMixin, View):
-    """
-        Step 2 View for uploading files for analysis
-    """
-    template_name = "main/step2.html"
-
-    def post(self, request):
-        # create new subfolder with bucket
-        bucket_name = request.POST['bucket']
-        bucket = Bucket.objects.get(name=bucket_name)
-        subfolder = SubFolder(bucket=bucket)
-
-        # get aws credentials from iam
-        iam = IAM.objects.filter(user=request.user).first()
-        secret_key = b64encode(b64encode(iam.aws_secret_access_key.encode('utf-8'))).decode("utf-8")
-        access_id = b64encode(b64encode(iam.aws_access_key.encode('utf-8'))).decode("utf-8")
-
-        return render(request=request, template_name=self.template_name, context={
-            "subfolder": subfolder,
-            "bucket": bucket_name,
-            "id1": access_id,
-            "id2": secret_key,
-        })
-
-
-class ProcessView(LoginRequiredMixin, View):
-    """
-        Step 3 View for processing files
-    """
-    template_name = "main/result.html"
-
-    def get(self, request):
-        return render(request=request, template_name=self.template_name)
-
-    def post(self, request):
-
-        iam = IAM.objects.filter(user=request.user).first()
-        secret_key = b64encode(b64encode(iam.aws_secret_access_key.encode('utf-8'))).decode("utf-8")
-        access_id = b64encode(b64encode(iam.aws_access_key.encode('utf-8'))).decode("utf-8")
-        buckets = Bucket.objects.all().order_by('name')
-
-        return render(request=request, template_name=self.template_name, context={
-            "id1": access_id,
-            "id2": secret_key,
-            "buckets": buckets,
-        })
-
-
-class ResultView(LoginRequiredMixin, View):
-    """
-        View for showing processing results
-    """
-    template_name = "main/result.html"
-
-    def get(self, request):
-
-        return render(request=request, template_name=self.template_name)
-
-
-class CheckProcessView(View):
-    """
-    View for checking processing progress
-    """
-
-    def get(self, request):
-        iam = IAM.objects.filter(user=request.user).first()
-        process_name = request.GET['process']
-        proc = Process.objects.get(name=process_name)
-        res = check_process(iam=iam, process=proc)
-        return JsonResponse({"status": True})
-        # return JsonResponse({"status": res})
-
+"""
+    Views for Demo page 
+"""
 
 mp4_file = "cunninghamlabEPI/results/jobepi_demo/hp_optimum/epi_opt.mp4"
 cert_file = "cunninghamlabEPI/results/jobepi_demo/logs/certificate.txt"
+dataset_folder = "cunninghamlabEPI/inputs/epidata"
+
 bucket_name = "epi-ncap"
 
 
