82c01dd06389d8bfbdda75b0c75ccabcd5dea959
upworka0 <upworka0@gmail.com>
2020-03-09 21:43:01 -0600

    change checkbox to radio buttons

--- static/js/demo_utils.js
+++ static/js/demo_utils.js
@@ -1,7 +1,7 @@
 // show dataset and config file list
 
-function get_tr_template(file){
-	return '<tr><td>' + file + '</td><td><input type="checkbox"></td>';
+function get_tr_template(file, type, name){
+	return '<tr><td>' + file + '</td><td><input type="' + type + '" name="' + name + '"></td>';
 }
 
 function refresh_databucket_list(){
@@ -17,23 +17,33 @@ function refresh_databucket_list(){
 			if (res.status == 200){
 				var dataset_html = '';
 				for ( var i = 0 ; i < res.datasets.length ; i++)
-					dataset_html += get_tr_template(res.datasets[i])
+					dataset_html += get_tr_template(res.datasets[i], "checkbox", "dataset_file")
 
 				dataset_html === '' ? $('#dataset_table tbody').html(empty_template) : $('#dataset_table tbody').html(dataset_html);
 
 				var config_html = '';
 				for ( var i = 0 ; i < res.configs.length ; i++)
-					config_html += get_tr_template(res.configs[i])
+					config_html += get_tr_template(res.configs[i], "radio", "config_file")
 
                 config_html === '' ? $('#config_table tbody').html(empty_template) : $('#config_table tbody').html(config_html);
 
 				// add eventlistener for each tr
 				$('tr').click(function(event){
-                    if (event.target.type !== 'checkbox') {
+					if (event.target.type !== 'checkbox') {
                         $(':checkbox', this).trigger('click');
+                    }
+
+                    if (event.target.type !== 'radio') {
+                        $(':radio', this).trigger('click');
                     }
 				})
 			}
 		}
 	})
+}
+
+function submit(){
+    var dataset_files = [];
+    var config_file = null;
+
 }
