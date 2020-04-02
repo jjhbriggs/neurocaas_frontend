47340d19d0d8496ea9534f7319638a321944ee56
upworka0 <upworka0@gmail.com>
2020-04-02 04:55:21 -0600

    added file hirachy

---
+++ static/js/demo.js
@@ -0,0 +1,96 @@
+
+/* get file name from url */
+function get_file_name(url){
+    return url === null ? "" : url.split("/").slice(-1)[0]
+}
+
+/* function to show processing status from certificate.txt */
+function get_status(timestamp){
+    url = "/demo_result/?timestamp=" + timestamp;
+    $.ajax({
+        url: url,
+        success: function(res){
+            console.log(res);
+            $('#status-text').html(res.cert_file);
+            if (res.dtset_logs.length > 0 ){
+                var html = "";
+                for ( i = 0; i < res.dtset_logs.length; i++){
+                    html += '<div class="media"><p class="media-body mb-0 small lh-125 border-bottom border-gray">';
+                    html += '<a href="../' + res.dtset_logs[i] + '" class="text-warning" target="_blank">';
+                    html += get_file_name(res.dtset_logs[i]) + '</a></p></div>';
+                }
+                $('#dtset-result-view').html(html);
+                $('.result-dataset').css('display', 'block');
+            }
+
+            if (processing_status)
+                setTimeout(function(){
+                    get_status(timestamp)
+                }, 30000);
+        }
+    })
+}
+
+/* function to show processing results from log and hp_optimum folder */
+function get_results(timestamp){
+    url = "/demo_result/";
+    $.ajax({
+        url: url,
+        method: "POST",
+        data: {
+            timestamp: timestamp
+        },
+        success: function(res){
+            console.log(res);
+            if(res.dtset_logs.length > 0) {
+                // append the log result to log table
+                var html = "";
+                for ( i = 0; i < res.dtset_logs.length; i++){
+                    html += '<div class="media"><p class="media-body mb-0 small lh-125 border-bottom border-gray">';
+                    html += '<a href="../' + res.dtset_logs[i] + '" class="text-warning" target="_blank">';
+                    html += get_file_name(res.dtset_logs[i]) + '</a></p></div>';
+                }
+                $('#dtset-result-view').html(html);
+            }
+
+            if(res.video_link) {
+                processing_status = false;
+                $('.spinner').css('display', 'none');
+                $('.result-video').css('display', 'block');
+
+                // show video result
+                html = '<div class="media"><p class="media-body mb-0 small lh-125 border-bottom border-gray">';
+                html += '<a href="../' + res.video_link + '" class="text-warning" target="_blank">';
+                html += get_file_name(res.video_link) + '</a></p></div>';
+
+                html += '<div class="media"><p class="media-body mb-0 small lh-125 border-bottom border-gray">';
+                html += '<a href="../' + res.csv_link + '" class="text-warning" target="_blank">';
+                html += get_file_name(res.csv_link) + '</a></p></div>';
+
+                $('#video-result-view').html(html);
+            }
+
+            if (processing_status)
+                setTimeout(function(){
+                    get_results(timestamp)
+                }, 30000);
+        }
+    })
+}
+
+var trigger_function = function(timestamp){
+    $('.spinner').css('display', 'block');
+    setTimeout(function(){
+        // timestamp = Math.floor(Date.now()/1000);
+        get_status(timestamp);
+        get_results(timestamp);
+    }, 1000);
+}
+
+var submit_trigger = function(){
+    setTimeout(function(){
+        refresh_databucket_list();
+        dataset_area.clear_status();
+        config_area.clear_status();
+    }, 700);
+}
