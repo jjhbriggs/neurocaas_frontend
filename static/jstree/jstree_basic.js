f7d08fdbdd5402f74d784da6ad39e7c10d5f65b8
upworka0 <upworka0@gmail.com>
2020-04-13 11:39:29 -0600

    added jstree

--- static/jstree/jstree_basic.js
+++ static/jstree/jstree_basic.js
@@ -18,22 +18,33 @@ function get_json_from_array(arr){
     return objectArray;
 }
 
-var paths = [
-    '/results/process_results/main_result.txt',
-    '/results/process_results/supp_results/supp_analysis_overall.txt',
-    '/results/process_results/supp_results/supp_diagnostic.txt',
-    '/logs/log1.txt',
-    '/logs/log2.txt',
-]
+function create_jstree(paths){
+    $('#hierarchy').remove();
+    console.log('removed');
+    $("#hierarchy_div").append('<div id="hierarchy"></div>');
+    $('#hierarchy')
+        .on("changed.jstree", function (e, data) {
+            if(data.selected.length) {
+                alert('The selected node is: ' + data.instance.get_node(data.selected[0]).text);
+            }
+        })
+        .jstree({
+            'core' : {
+                'data' : get_json_from_array(paths)
+            }
+        });
+}
 
-$('#hierarchy')
-    .on("changed.jstree", function (e, data) {
-		if(data.selected.length) {
-			alert('The selected node is: ' + data.instance.get_node(data.selected[0]).text);
-		}
-	})
-	.jstree({
-		'core' : {
-			'data' : get_json_from_array(paths)
-		}
-	});
+function update_jstree(){
+    var paths = [];
+
+    dtset_logs.forEach(function(item){
+        paths.push('/results/' + item.path);
+    })
+
+    results_links.forEach(function(item){
+        paths.push('/results/' + item.path);
+    })
+
+    create_jstree(paths);
+}
