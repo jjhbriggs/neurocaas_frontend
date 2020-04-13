fd52d058bbc46a4ad1b8eb6fb683dbb2c7e3339d
upworka0 <upworka0@gmail.com>
2020-04-12 21:23:18 -0600

    working in file structures

--- static/js/demo_utils.js
+++ static/js/demo_utils.js
@@ -1,7 +1,22 @@
 // show dataset and config file list
 
-function get_tr_template(file, type, name){
-	return '<tr><td class="value">' + file + '</td><td><input type="' + type + '" name="' + name + '"></td>';
+function get_tr_template(file, type, name, ind, flag=0){
+	return '<tr onclick="show_detail(' + ind + ', ' + flag + ')"><td class="value">' + file + '</td><td><input type="' + type + '" name="' + name + '"></td>';
+}
+
+function show_detail(ind, type){
+    if (processing_status) return;
+    var content = "";
+    if (type === 0){
+        content += 'Name: ' + datasets[ind].name + '\n\n';
+        content += 'Size: ' + datasets[ind].size + '\n\n';
+        content += 'Date modified: ' + datasets[ind].date_modified + '\n';
+    } else {
+        content += 'Name: ' + configs[ind].name + '\n\n';
+        content += 'Date modified: ' + configs[ind].date_modified + '\n\n';
+        content += configs[ind].content + '\n';
+    }
+    $('#status-text').html(content)
 }
 
 function refresh_databucket_list(){
@@ -20,13 +35,13 @@ function refresh_databucket_list(){
 
 				var dataset_html = '';
 				for ( var i = 0 ; i < datasets.length ; i++)
-					dataset_html += get_tr_template(datasets[i].name, "checkbox", "dataset_file")
+					dataset_html += get_tr_template(datasets[i].name, "checkbox", "dataset_file", i, 0)
 
 				dataset_html === '' ? $('#dataset_table tbody').html(empty_template) : $('#dataset_table tbody').html(dataset_html);
 
 				var config_html = '';
 				for ( var i = 0 ; i < configs.length ; i++)
-					config_html += get_tr_template(configs[i].name, "radio", "config_file")
+					config_html += get_tr_template(configs[i].name, "radio", "config_file", i, 1)
 
                 config_html === '' ? $('#config_table tbody').html(empty_template) : $('#config_table tbody').html(config_html);
 
@@ -40,8 +55,9 @@ function refresh_databucket_list(){
                         $(':radio', this).trigger('click');                        
                     }                    
 
-                    if ($(this).find('input').is(":checked") === true)
+                    if ($(this).find('input').is(":checked") === true){
                     	$(this).addClass('active');
+                    }
                     else
                     	$(this).removeClass('active');
 
@@ -83,6 +99,10 @@ function submit(){
     config_file = radiobox[0].textContent;
 
     $('#btn-spinner').css('display', 'inline-block');
+
+    // set the processing status to TRUE
+    processing_status = true;
+
     $.ajax({
     	url: window.location.pathname,
     	method: 'POST',
