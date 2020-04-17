1d6d41dfc97c02bb0b6f990fa5af160791135576
upworka0 <upworka0@gmail.com>
2020-04-17 04:51:53 -0600

    change route

--- main/urls.py
+++ main/urls.py
@@ -5,10 +5,10 @@ from .views import *
 urlpatterns = [
     path('', IntroView.as_view(), name='intro'),
     path('home/', HomeView.as_view(template_name="main/home.html"), name="home"),
-    path('intro/', AnalysisIntroView.as_view(), name='analysis_intro'),
+    path('intro/<int:id>', AnalysisIntroView.as_view(), name='analysis_intro'),
 
     # demo 2
-    path('process/', ProcessView.as_view(), name='process'),
+    path('process/<int:id>', ProcessView.as_view(), name='process'),
     path('get_user_files/', UserFilesView.as_view(), name='get_user_files'),
     path('get_results/', ResultView.as_view(), name='get_results')
 ]
