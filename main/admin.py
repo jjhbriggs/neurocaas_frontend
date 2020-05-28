63a77304c9788781f98b84d43fb503149e1e0319
upworka0 <upworka0@gmail.com>
2020-05-27 19:49:13 -0700

    feat: added see all link for analysis list

--- main/admin.py
+++ main/admin.py
@@ -3,28 +3,7 @@ from .models import *
 
 # Register your models here.
 
-"""
-@admin.register(Bucket)
-class BucketAdmin(admin.ModelAdmin):
-    list_display = ('name', 'algorithrm', 'description', 'created_on',)
-
-
-@admin.register(FileItem)
-class FileItemAdmin(admin.ModelAdmin):
-    list_display = ('name', 'link', 'uploaded', 'created_on',)
-
-
-@admin.register(Process)
-class ProcessAdmin(admin.ModelAdmin):
-    list_display = ('name', 'iam', 's3_path', 'created_on',)
-
-
-@admin.register(SubFolder)
-class SubFolderAdmin(admin.ModelAdmin):
-    list_display = ('name', 'bucket', 'created_on', )
-"""
-
 
 @admin.register(Analysis)
 class SubFolderAdmin(admin.ModelAdmin):
-    list_display = ('analysis_name', 'bucket_name', 'short_description')
+    list_display = ('analysis_name', 'bucket_name', 'short_description', 'custom')
