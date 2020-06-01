ea94a9d62a1e30e1c34b329dbecce18f4aa25025
upworka0 <upworka0@gmail.com>
2020-06-01 03:50:57 -0700

    feat: 7: working on adding job history

--- main/urls.py
+++ main/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     path('', IntroView.as_view(), name='intro'),
     path('home/', HomeView.as_view(template_name="main/home.html"), name="home"),
     path('intro/<int:id>', AnalysisIntroView.as_view(), name='analysis_intro'),
+    path('history/<int:ana_id>', JobHistoryListView.as_view(), name='job_history'),
+    path('history/<int:ana_id>/<int:job_id>', JobDetailView.as_view(), name='job_detail'),
     path('analysis_qa/', QAView.as_view(), name='analysis_qa'),
     path('analyses/', AnalysisListView.as_view(), name='analyses'),
 
