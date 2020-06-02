86a5b4729a1eb58dd43830bc1a37afd2c292aedc
upworka0 <upworka0@gmail.com>
2020-06-02 04:06:45 -0700

    working on job history view

--- main/urls.py
+++ main/urls.py
@@ -5,11 +5,11 @@ from .views import *
 urlpatterns = [
     path('', IntroView.as_view(), name='intro'),
     path('home/', HomeView.as_view(template_name="main/home.html"), name="home"),
+    path('analyses/', AnalysisListView.as_view(), name='analyses'),
     path('analysis/<int:ana_id>', AnalysisIntroView.as_view(), name='analysis_intro'),
+    path('freq_qa/', QAView.as_view(), name='frequent_qa'),
     path('history/<int:ana_id>', JobHistoryListView.as_view(), name='job_history'),
     path('history/<int:ana_id>/<str:job_id>', JobDetailView.as_view(), name='job_detail'),
-    path('analysis_qa/', QAView.as_view(), name='analysis_qa'),
-    path('analyses/', AnalysisListView.as_view(), name='analyses'),
 
     path('process/<int:id>', ProcessView.as_view(), name='process'),
     path('get_user_files/<int:ana_id>', UserFilesView.as_view(), name='get_user_files'),
