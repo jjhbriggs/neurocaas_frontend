43dcd4189db18db2462d5c4867ee7e22f04f17ee
upworka0 <upworka0@gmail.com>
2020-06-02 22:45:54 -0700

    remove unnecessary code

--- main/urls.py
+++ main/urls.py
@@ -13,6 +13,6 @@ urlpatterns = [
     path('files/<int:ana_id>/', FilesView.as_view(), name='file_view'),
 
     path('process/<int:id>', ProcessView.as_view(), name='process'),
-    path('get_user_files/<int:ana_id>', UserFilesView.as_view(), name='get_user_files'),
-    path('get_results/', ResultView.as_view(), name='get_results')
+    path('user_files/<int:ana_id>', UserFilesView.as_view(), name='get_user_files'),
+    path('results/', ResultView.as_view(), name='get_results')
 ]
