72d9527c087bf0903f2f6a6b6cef321cf91dab0b
upworka0 <upworka0@gmail.com>
2020-06-03 23:29:48 -0700

    reformat js files

---
+++ static/js/utils.js
@@ -0,0 +1,46 @@
+/* get file name from url */
+function get_file_name(url){
+    return url === null ? "" : url.split("/").slice(-1)[0]
+}
+
+
+function hide_spinner(){
+    $('#spinner').css('display', 'none');
+}
+
+
+function show_spinner(){
+    $('#spinner').css('display', 'block');
+}
+
+
+// Async Ajax Request
+function AjaxRequest(url, method='GET', data=null){
+    /*
+        Async Ajax Module
+
+        @params:
+                url: endpoint,
+                data: array,
+                method: request method
+        @return:
+                array
+    */
+    show_spinner();
+    return new Promise((resolve, reject) => {
+       $.ajax({
+            url : url,
+            method : method,
+            data : data,
+            success :  function(res){
+                hide_spinner();
+                resolve(res);
+            },
+            error: function(err){
+                hide_spinner();
+                reject();
+            }
+       });
+    });
+}
+
