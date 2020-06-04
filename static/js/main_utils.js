05df359281f6850a415858d081ce8c74bd76f209
upworka0 <upworka0@gmail.com>
2020-06-03 19:10:10 -0700

    remove unnecessary code in main_utils

--- static/js/main_utils.js
+++ static/js/main_utils.js
@@ -1,8 +1,3 @@
-// show dataset and config file list
-function get_tr_template(file, type, name, ind, flag=0){
-	return '<tr onclick="show_detail(' + ind + ', ' + flag + ')"><td class="value">' + file + '</td><td><input type="' + type + '" name="' + name + '"></td>';
-}
-
 /* get file name from url */
 function get_file_name(url){
     return url === null ? "" : url.split("/").slice(-1)[0]
@@ -88,39 +83,6 @@ async function refresh_bucket(){
         configs = res.configs;
         data_sets = res.data_sets;
 
-        var data_set_html = '';
-        for ( var i = 0 ; i < data_sets.length ; i++)
-            data_set_html += get_tr_template(data_sets[i].name, "checkbox", "dataset_file", i, 0)
-
-        data_set_html === '' ? $('#data_set_table tbody').html(empty_template) : $('#data_set_table tbody').html(data_set_html);
-
-        var config_html = '';
-        for ( var i = 0 ; i < configs.length ; i++)
-            config_html += get_tr_template(configs[i].name, "radio", "config_file", i, 1)
-
-         config_html === '' ? $('#config_table tbody').html(empty_template) : $('#config_table tbody').html(config_html);
-
-        // add eventlistener for each tr
-        $('tr').click(function(event){
-            if (event.target.type !== 'checkbox') {
-                $(':checkbox', this).trigger('click');                        
-            }
-
-            if (event.target.type !== 'radio') {
-                $(':radio', this).trigger('click');                        
-            }                    
-
-            if ($(this).find('input').is(":checked") === true){
-                $(this).addClass('active');
-            }
-            else
-                $(this).removeClass('active');
-
-            if ($(this).find('input[type="radio"]').val() === 'on'){
-                $('input[type="radio"]').parent().parent().removeClass('active');
-                $(this).addClass('active');
-            }
-        })
 
         refresh_data_jstrees();
 
