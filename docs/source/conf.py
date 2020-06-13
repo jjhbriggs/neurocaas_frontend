3618c0bda015f2d9529826b9e1af162de427a7fe
jjhbriggs <jjhbriggs@gmail.com>
2020-06-13 19:37:35 +0000

    sphinx config changes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,8 +12,9 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('~/ncap/account'))
-
+sys.path.insert(0, os.path.abspath('~/ncap'))
+from django.conf import settings
+settings.configure()
 
 # -- Project information -----------------------------------------------------
 
