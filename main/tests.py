5c5f717ee47e8ded82f5d3a29039329345b5f333
jjhbriggs <jjhbriggs@gmail.com>
2020-07-07 22:12:58 +0000

    adding some main tests back

--- main/tests.py
+++ main/tests.py
@@ -5,90 +5,90 @@ from .models import Analysis
 from account.models import *
 
 
-# class AnalysisTestCase(TestCase):
-#     """Class for testing IAM connected to analyses."""
-#     def setUp(self):
-#         """Setup user, group, IAM, and analysis"""
-#         user = User.objects.create_user('test1@test.com', password='test')
-#         user.first_name = "Test1"
-#         user.last_name = "User"
-#         user.save()
-#         group = AnaGroup.objects.create(name="test group")
-#         self.iam = IAM.objects.create(user=user,
-#                                       aws_user="AWS user",
-#                                       aws_access_key="AWS access key",
-#                                       aws_secret_access_key="AWS secret key",
-#                                       group=group)
-# 
-#         analysis = Analysis.objects.create(
-#             analysis_name="Test Analysis",
-#             result_prefix="test_prefix",
-#             bucket_name="Test bucket",
-#             custom=True,
-#             short_description="Short Description",
-#             long_description="Long Description",
-#             paper_link="Paper Link",
-#             git_link="Github Link",
-#             bash_link="Bash Script Link",
-#             demo_link="Demo page link",
-#             signature="Signature"
-#         )
-# 
-#         analysis.groups.add(group)
-# 
-#     def test_check_iam_with_analysis(self):
-#         """Test the current IAM is the same which is associated with the analysis request."""
-# 
-#         analysis = Analysis.objects.get(bucket_name='Test bucket')
-#         iam = IAM.objects.get(aws_user='AWS user')
-#         self.assertIs(analysis.check_iam(iam), True)
-# 
-# 
-# class AnalysisListViewTest(TestCase):
-#     """Class for testing the list view of analyses."""
-#     def setUp(self):
-#         """Setup two groups and analyses"""
-#         group1 = AnaGroup.objects.create(name="test group1")
-#         group2 = AnaGroup.objects.create(name="test group2")
-#         analysis1 = Analysis.objects.create(
-#             analysis_name="Test Analysis1",
-#             result_prefix="test_prefix1",
-#             bucket_name="Test bucket1",
-#             custom=False,
-#             short_description="Short Description1",
-#             long_description="Long Description1",
-#             paper_link="Paper Link1",
-#             git_link="Github Link1",
-#             bash_link="Bash Script Link1",
-#             demo_link="Demo page link1",
-#             signature="Signature1"
-#         )
-#         analysis1.groups.add(group1)
-# 
-#         analysis2 = Analysis.objects.create(
-#             analysis_name="Test Analysis2",
-#             result_prefix="test_prefix2",
-#             bucket_name="Test bucket2",
-#             custom=True,
-#             short_description="Short Description2",
-#             long_description="Long Description2",
-#             paper_link="Paper Link2",
-#             git_link="Github Link2",
-#             bash_link="Bash Script Link2",
-#             demo_link="Demo page link2",
-#             signature="Signature2"
-#         )
-#         analysis2.groups.add(group2)
-# 
-#     def test_analysis_list_view(self):
-#         """Check that the the two analyses are displayed correctly."""
-#         response = self.client.get('/analyses/')
-#         #print(response.context)
-#         #print("hi")
-#         self.assertQuerysetEqual(response.context['main_analyses'], ['<Analysis: Test Analysis1>'])
-#         #print(response.context['main_analyses'])
-#         self.assertQuerysetEqual(response.context['custom_analyses'], ['<Analysis: Test Analysis2>'])
-#         self.assertEqual(response.context['iam'], None)
+class AnalysisTestCase(TestCase):
+    """Class for testing IAM connected to analyses."""
+    def setUp(self):
+        """Setup user, group, IAM, and analysis"""
+        user = User.objects.create_user('test1@test.com', password='test')
+        user.first_name = "Test1"
+        user.last_name = "User"
+        user.save()
+        group = AnaGroup.objects.create(name="test group")
+        self.iam = IAM.objects.create(user=user,
+                                      aws_user="AWS user",
+                                      aws_access_key="AWS access key",
+                                      aws_secret_access_key="AWS secret key",
+                                      group=group)
+
+        analysis = Analysis.objects.create(
+            analysis_name="Test Analysis",
+            result_prefix="test_prefix",
+            bucket_name="Test bucket",
+            custom=True,
+            short_description="Short Description",
+            long_description="Long Description",
+            paper_link="Paper Link",
+            git_link="Github Link",
+            bash_link="Bash Script Link",
+            demo_link="Demo page link",
+            signature="Signature"
+        )
+
+        analysis.groups.add(group)
+
+    def test_check_iam_with_analysis(self):
+        """Test the current IAM is the same which is associated with the analysis request."""
+
+        analysis = Analysis.objects.get(bucket_name='Test bucket')
+        iam = IAM.objects.get(aws_user='AWS user')
+        self.assertIs(analysis.check_iam(iam), True)
+
+
+class AnalysisListViewTest(TestCase):
+    """Class for testing the list view of analyses."""
+    def setUp(self):
+        """Setup two groups and analyses"""
+        group1 = AnaGroup.objects.create(name="test group1")
+        group2 = AnaGroup.objects.create(name="test group2")
+        analysis1 = Analysis.objects.create(
+            analysis_name="Test Analysis1",
+            result_prefix="test_prefix1",
+            bucket_name="Test bucket1",
+            custom=False,
+            short_description="Short Description1",
+            long_description="Long Description1",
+            paper_link="Paper Link1",
+            git_link="Github Link1",
+            bash_link="Bash Script Link1",
+            demo_link="Demo page link1",
+            signature="Signature1"
+        )
+        analysis1.groups.add(group1)
+
+        analysis2 = Analysis.objects.create(
+            analysis_name="Test Analysis2",
+            result_prefix="test_prefix2",
+            bucket_name="Test bucket2",
+            custom=True,
+            short_description="Short Description2",
+            long_description="Long Description2",
+            paper_link="Paper Link2",
+            git_link="Github Link2",
+            bash_link="Bash Script Link2",
+            demo_link="Demo page link2",
+            signature="Signature2"
+        )
+        analysis2.groups.add(group2)
+
+    def test_analysis_list_view(self):
+        """Check that the the two analyses are displayed correctly."""
+        response = self.client.get('/analyses/')
+        #print(response.context)
+        #print("hi")
+        self.assertQuerysetEqual(response.context['main_analyses'], ['<Analysis: Test Analysis1>'])
+        #print(response.context['main_analyses'])
+        self.assertQuerysetEqual(response.context['custom_analyses'], ['<Analysis: Test Analysis2>'])
+        self.assertEqual(response.context['iam'], None)
 
 
 # class AnalysisIntroViewTest(TestCase):
