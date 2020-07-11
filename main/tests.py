14e451bfcdd9860a4b6965b289b1ef3dce0f1147
jjhbriggs <jjhbriggs@gmail.com>
2020-07-11 01:08:58 +0000

    job detail view test

--- main/tests.py
+++ main/tests.py
@@ -4,6 +4,7 @@ from django.test import TestCase
 from .models import Analysis
 from account.models import *
 import os
+from .utils import *
 
 class AnalysisTestCase(TestCase):
     """Class for testing IAM connected to analyses."""
@@ -180,12 +181,88 @@ class JobListViewTest(TestCase):
     def test_job_list_view(self):
         """Check that history of user's analyses are displayed properly."""
         
+        results_folder = '%s/results' % self.group
+        job_list = get_job_list(iam=self.iam, bucket=self.analysis.bucket_name, folder=results_folder)
         response = self.client.get('/history/%s' % self.analysis.id)
         self.assertEqual(response.context['analysis'], self.analysis)
         self.assertEqual(response.context['iam'], self.iam)
+        self.assertEqual(response.context['job_list'], job_list)
     def test_no_perms_job_list_view(self):
         """Check that history of user's analyses is not displayed if the user doesn't have permission to access to the analysis."""
         
         response = self.client.get('/history/%s' % self.analysis2.id)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['Location'], '/')
+        
+class JobDetailViewTest(TestCase):
+    """Class for testing the job detail view."""
+    def setUp(self):
+        """Setup user, group, IAM, and analysis. Login IAM."""
+        self.user = User.objects.create_user('test@test.com', password='test')
+        self.user.first_name = "Jack"
+        self.user.last_name = "Briggs"
+        self.user.save()
+        self.group = AnaGroup.objects.create(name="reviewers")
+        self.iam = IAM.objects.create(user=self.user,
+                                      aws_user="jbriggs",
+                                      aws_access_key=os.environ.get('AWS_ACCESS_KEY'),
+                                      aws_secret_access_key=os.environ.get('AWS_SECRET_ACCESS_KEY'),
+                                      group=self.group)
+        self.analysis = Analysis.objects.create(
+            analysis_name="Test Analysis",
+            result_prefix="job__epi-ncap-web_",
+            bucket_name="epi-ncap-web",
+            custom=False,
+            short_description="Short Description",
+            long_description="Long Description",
+            paper_link="Paper Link",
+            git_link="Github Link",
+            bash_link="Bash Script Link",
+            demo_link="Demo page link",
+            signature="Signature"
+        )
+        self.analysis.groups.add(self.group)
+        self.analysis2 = Analysis.objects.create(
+            analysis_name="Test Analaysis (No perms)",
+            result_prefix="job__epi-ncap-web_",
+            bucket_name="epi-ncap-web",
+            custom=False,
+            short_description="Short Description",
+            long_description="Long Description",
+            paper_link="Paper Link",
+            git_link="Github Link",
+            bash_link="Bash Script Link",
+            demo_link="Demo page link",
+            signature="Signature"
+        )
+        # login here
+        form = {
+            'email': 'test@test.com',
+            'password': 'test',
+        }
+        r = self.client.post('/login/', form)
+        
+        res_folder = '%s/results' % self.group
+        job_list = get_job_list(iam=self.iam, bucket=self.analysis.bucket_name, folder=res_folder)
+        
+        self.job_id = job_list[0]['name']
+    def test_job_detail_view(self):
+        """Check that detail of a user's previous analysis are displayed properly."""
+        
+        response = self.client.get('/history/' + str(self.analysis.id) + '/' + self.job_id)
+        result_folder = "%s/results/%s" % (self.group.name, self.job_id)
+        result_keys = get_list_keys(iam=self.iam,
+                                    bucket=self.analysis.bucket_name,
+                                    folder=result_folder,
+                                    un_cert=False)
+        job_detail = [item.replace(result_folder, '/results') for item in result_keys]
+        
+        self.assertEqual(response.context['analysis'], self.analysis)
+        self.assertEqual(response.context['iam'], self.iam)
+        self.assertEqual(response.context['job_detail'], json.dumps(job_detail))
+    def test_no_perms_job_list_view(self):
+        """Check that detail of a user's previous analysis is not displayed if the user doesn't have permission to access to the analysis."""
+        
+        response = self.client.get('/history/' + str(self.analysis2.id) + '/' + self.job_id)
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response['Location'], '/')
