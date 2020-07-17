13b9a2a8ef850625ebda432f268e770c283155c9
jjhbriggs <jjhbriggs@gmail.com>
2020-07-17 20:55:23 +0000

    testing db clean job now uses s3 text file instead of local for storing prefixes

--- cleanup_test_results.py
+++ cleanup_test_results.py
@@ -6,16 +6,20 @@ s3 = boto3.resource('s3',
             aws_access_key_id=os.environ.get('AWS_ACCESS_KEY'),
             aws_secret_access_key=os.environ.get('AWS_SECRET_ACCESS_KEY'))
 bucket = s3.Bucket("cianalysispermastack")
+        
+object = s3.Object('cianalysispermastack', 'frontendtravisci/results/prefixes_for_delete.txt')
+content = object.get()['Body'].read().decode('utf-8') 
 
-with open("prefixes_for_delete.txt", "r+") as f:
-    content = f.readlines()
-    content = [x.strip() for x in content]
-    num = 0
-    for pref in content:
+content = content.split('\n')
+del content[-1]
+
+num = 0
+for pref in content:
+    for obj in bucket.objects.filter(Prefix=pref):
         num += 1
-        for obj in bucket.objects.filter(Prefix=pref):
-	        obj.delete()
-        print("Deleted " + pref)
-    if num == 0:
-        print("Nothing to remove")
-    f.truncate(0)
+        obj.delete()
+        print("Object found:" + str(obj))
+    print("Deleted " + pref)
+if num == 0:
+    print("Nothing to remove")
+object.put(Body="")
