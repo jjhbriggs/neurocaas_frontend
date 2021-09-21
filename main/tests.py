99191cc9582cb7fdaa53aa2890ed0f242108de9f
jjhbriggs <jjhbriggs@gmail.com>
2021-09-21 23:55:28 +0000

    test attmpt 4

--- main/tests.py
+++ main/tests.py
@@ -608,8 +608,11 @@ class ExtraUtilsTest(TestCase):
         f.close()
         ret = upload_file_to_s3(iam=self.iam, bucket=self.analysis.bucket_name, key=res_key, file_path="test.txt")
         print(ret)
-        res_folder = '%s/temp' % self.group
-        ret = download_file_from_s3(iam=self.iam, bucket=self.analysis.bucket_name, key="test.txt", folder=res_folder)
+
+        key = "temp/test.txt"
+        file_key = "%s/%s" % (self.group.name, key)
+        folder = generate_folder()
+        ret = download_file_from_s3(iam=self.iam, bucket=self.analysis.bucket_name, key=file_key, folder=folder)
         print(ret)
         self.assertIsNotNone(ret)
         res_folder = '%s/temp' % self.group
