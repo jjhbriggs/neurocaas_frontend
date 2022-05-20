d065b657ca61ec46c156e7dd270555ba03e6a5a1
jjhbriggs <jjhbriggs@gmail.com>
2022-05-20 18:52:03 +0000

    removing ns workaround

--- aws_sdk_examples/sts.py
+++ aws_sdk_examples/sts.py
@@ -22,24 +22,8 @@ import ctypes
 
 CLOCK_REALTIME = 0
 
-#: These two functions are for enavling time_ns
-class timespec(ctypes.Structure):
-    _fields_ = [
-        ('tv_sec', ctypes.c_int64), # seconds, https://stackoverflow.com/q/471248/1672565
-        ('tv_nsec', ctypes.c_int64), # nanoseconds
-        ]
-
-clock_gettime = ctypes.cdll.LoadLibrary('libc.so.6').clock_gettime
-clock_gettime.argtypes = [ctypes.c_int64, ctypes.POINTER(timespec)]
-clock_gettime.restype = ctypes.c_int64    
-
-def time_ns():
-    tmp = timespec()
-    ret = clock_gettime(CLOCK_REALTIME, ctypes.pointer(tmp))
-    if bool(ret):
-        raise OSError()
-    return tmp.tv_sec * 10 ** 9 + tmp.tv_nsec
-
+def time_millis():
+    return int((datetime.datetime.utcnow() - datetime.datetime(1970, 1, 1)).total_seconds() * 1000)
 def progress_bar(seconds):
     """Shows a simple progress bar in the command window."""
     for _ in range(seconds):
@@ -50,7 +34,7 @@ def progress_bar(seconds):
 
 
 def unique_name(base_name):
-    return f'demo-assume-role-{base_name}-{time_ns()}'
+    return f'sts-{base_name}-{time_millis()}'
 
 
 def setup(iam_resource):
