2ec219699647756279c9fcf4f1e77ec01ff0e1be
jjhbriggs <jjhbriggs@gmail.com>
2022-06-14 00:23:34 +0000

    unit tests

--- ncap/settings.py
+++ ncap/settings.py
@@ -13,11 +13,12 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 import os
 import environ
 
-env = environ.Env()
-environ.Env.read_env()
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
